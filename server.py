878b8bd79b8c05c175c195a6b36d5b95931e702c
Jamey-Dogom <33010705+AppJedi@users.noreply.github.com>
2019-07-10 13:54:28 -0700

    registering new users accomplished with data stored in db

--- server.py
+++ server.py
@@ -2,38 +2,45 @@ from flask import Flask, render_template, redirect, request, session, flash
 from mysqlconnection import connectToMySQL    # import the function that will return an instance of a connection
 import re
 import os
+from flask_bcrypt import Bcrypt  
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+bcrypt = Bcrypt(app)     # we are creating an object called bcrypt, 
+                         # which is made by invoking the function Bcrypt with our app as an argument
+
+
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
     
 @app.route("/")
 def index():
-    mysql = connectToMySQL('emails')	        # call the function, passing in the name of our db
-    all_emails = mysql.query_db('SELECT * FROM email_table;')  # call the query_db function, pass in the query as a string
-    return render_template("/index.html", all_emails = all_emails)
+    session = []
+    # mysql = connectToMySQL('site_users')	        # call the function, passing in the name of our db
+    # mysql.query_db('SELECT * FROM site_user;')  # call the query_db function, pass in the query as a string
+    return render_template("/index.html")
 
-@app.route("/create_email", methods = ["POST"])
+@app.route("/create_user", methods = ["POST"])
 def create_user():
     session['result'] = []
-    if not EMAIL_REGEX.match(request.form['user_email']):    # test whether a field matches the pattern
+    if not EMAIL_REGEX.match(request.form['email']):    # test whether a field matches the pattern
         flash("Email is not valid!")
         return redirect("/")
 
-    mysql = connectToMySQL('emails')
-    query = "INSERT INTO email_table (email) VALUES (%(mail)s);"
+    mysql = connectToMySQL('site_users')
+    query = "INSERT INTO site_user (first_name, last_name, email, password) VALUES (%(fn)s, %(ln)s, %(mail)s, %(pw)s);"
 
     data = {
-        "mail": request.form['user_email']
+        "fn": request.form['first_name'],
+        "ln": request.form['last_name'],
+        "mail": request.form['email'],
+        "pw": request.form['password']
     }
 
-    ent = request.form['user_email']
+    print(data)
 
-    new_player = mysql.query_db(query, data)
-    mysql = connectToMySQL('emails')
-    all_emails = mysql.query_db('SELECT * FROM email_table;')  
-    return render_template("/success.html", all_emails = all_emails, ent = ent)
+    mysql.query_db(query, data)
+    return render_template("/success.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
